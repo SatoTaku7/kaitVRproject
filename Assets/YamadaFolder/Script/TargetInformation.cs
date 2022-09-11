f44461f4e2e5cda827d718464f20656864bb69ce
Yyuki78 <yuki.yamada2411@gmail.com>
2022-09-11 21:43:57 +0900

    再度的の生成・破壊をする仮プログラムを作成

---
+++ Assets/YamadaFolder/Script/TargetInformation.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetInformation : MonoBehaviour
+{
+    //的それぞれの情報　自分の番号、色、サイズを持つ
+
+    public int num;
+    public int color;
+    public int size;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
