80a53dc997cdada02eb5c333b07d3ca52a306b40
SatoTaku7 <98199671+SatoTaku7@users.noreply.github.com>
2022-10-02 17:29:05 +0900

    別色で撃った時に的が反応しない仕様の修正・パワーアップ時の仕様変更・スタートボタンの関数が1回だけ呼び出されるように修正

--- Assets/TakumiFolder/Scripts/VR_UI.cs
+++ Assets/TakumiFolder/Scripts/VR_UI.cs
@@ -18,25 +18,23 @@ public class VR_UI : MonoBehaviour
 
     void Update()
     {
-        if (gunManager.ButtonClicked)//スタートボタンが押されたらボタンが消える
-        {
-           // Debug.Log("ボタンクリック:"+gunManager.ButtonClicked);
-            if (Button.gameObject.name == gunManager.ButtonName)
-            {
-               // Debug.Log("ボタン名:" + gunManager.ButtonClicked);
-                Button.gameObject.SetActive(false);
-                stateChanger.ChangeState(IStateChanger.GameState.Game);
-            }
-        }
-
         if (stateChanger.currentState == IStateChanger.GameState.Title)//ゲームオーバーが呼ばれたらスタートボタンを出現
         {
-            Button.gameObject.SetActive(true);
+            Button.gameObject.SetActive(true);//タイトルの時はボタンを表示
+            ButtonClicked();//ボタンのクリックを検知した場合の関数…ボタンが非表示になりゲームのStateを変更する
         }
     }
 
     void ButtonClicked()
     {
-
+        if (gunManager.ButtonClicked==true)//スタートボタンが押されたらボタンが消える
+        {
+            if (Button.gameObject.name == gunManager.ButtonName)//クリックしたものがスタートボタンだったとき　ボタンが消えてゲームのStateが"Game"になる
+            {
+                // Debug.Log("ボタン名:" + gunManager.ButtonClicked);
+                Button.gameObject.SetActive(false);
+                stateChanger.ChangeState(IStateChanger.GameState.Game);
+            }
+        }
     }
 }
