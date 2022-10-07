fbde33a9a62d5c71668907f6aeff0082a4bac032
Yyuki78 <yuki.yamada2411@gmail.com>
2022-10-07 10:26:01 +0900

    コンボとスコアを表示するUIを作成＋色付き的の増やし方を変更

--- Assets/YamadaFolder/Script/ComboUIController.cs
+++ Assets/YamadaFolder/Script/ComboUIController.cs
@@ -29,6 +29,20 @@ public class ComboUIController : MonoBehaviour
             displayComboUI[i].fillAmount = 0;
         }
         StartCoroutine(loop());
+        StartCoroutine(loop2());
+        StartCoroutine(loop3());
+        StartCoroutine(loop4());
+        StartCoroutine(loop5());
+        StartCoroutine(loop6());
+        StartCoroutine(loop7());
+        StartCoroutine(loop8());
+        StartCoroutine(loop9());
+        StartCoroutine(loop10());
+        StartCoroutine(loop11());
+        StartCoroutine(loop12());
+        StartCoroutine(loop13());
+        StartCoroutine(loop14());
+        //StartCoroutine(loop15());
     }
 
     // Update is called once per frame
@@ -43,6 +57,7 @@ public class ComboUIController : MonoBehaviour
             coroutine = StartCoroutine("coolDown");
             once = false;
         }
+        waitTime = 0.3f - (comboNum / 50f);
     }
 
     private IEnumerator coolDown()
@@ -59,36 +74,186 @@ public class ComboUIController : MonoBehaviour
 
     private IEnumerator loop()
     {
+        yield return new WaitForSeconds(0.14f);
         while (true)
         {
             if (breakTarget)
             {
-                minY = 0.1f + (comboNum / 30f) + (plusNum * 0.75f);
-                maxY = 0.225f + (comboNum / 15f) + plusNum;
+                minY = 0.05f + (comboNum / 50f) + (plusNum * 0.5f);
+                maxY = Mathf.Min(0.225f + (comboNum / 15f) + plusNum, 1f);
             }
             else
             {
-                minY = 0.1f + (comboNum / 30f);
+                minY = 0.01f + (comboNum / 50f);
                 maxY = 0.225f + (comboNum / 15f);
             }
-
-            if (maxY > 1)
-                maxY = 1;
+            
 
             firstAmountY = Random.Range(minY, maxY);
 
             displayComboUI[0].fillAmount = firstAmountY;
+            
+            yield return new WaitForSeconds(waitTime / 2);
+        }
+    }
+
+    private IEnumerator loop2()
+    {
+        yield return new WaitForSeconds(0.13f);
+        StartCoroutine(loop3());
+        while (true)
+        {
+            displayComboUI[1].fillAmount = displayComboUI[0].fillAmount;
+            yield return new WaitForSeconds(waitTime / 2);
+        }
+    }
+
+    private IEnumerator loop3()
+    {
+        yield return new WaitForSeconds(0.12f);
+        while (true)
+        {
+            displayComboUI[2].fillAmount = displayComboUI[1].fillAmount;
+            yield return new WaitForSeconds(waitTime / 2);
+        }
+    }
+
+    private IEnumerator loop4()
+    {
+        yield return new WaitForSeconds(0.11f);
+        while (true)
+        {
+            displayComboUI[3].fillAmount = displayComboUI[2].fillAmount;
+            yield return new WaitForSeconds(waitTime / 2);
+        }
+    }
+
+    private IEnumerator loop5()
+    {
+        yield return new WaitForSeconds(0.10f);
+        while (true)
+        {
+            displayComboUI[4].fillAmount = displayComboUI[3].fillAmount;
+            yield return new WaitForSeconds(waitTime / 2);
+        }
+    }
+
+    private IEnumerator loop6()
+    {
+        yield return new WaitForSeconds(0.09f);
+        while (true)
+        {
+            displayComboUI[5].fillAmount = displayComboUI[4].fillAmount;
+            yield return new WaitForSeconds(waitTime / 2);
+        }
+    }
+
+    private IEnumerator loop7()
+    {
+        yield return new WaitForSeconds(0.08f);
+        while (true)
+        {
+            displayComboUI[6].fillAmount = displayComboUI[5].fillAmount;
+            yield return new WaitForSeconds(waitTime / 2);
+        }
+    }
+
+    private IEnumerator loop8()
+    {
+        yield return new WaitForSeconds(0.07f);
+        while (true)
+        {
+            displayComboUI[7].fillAmount = displayComboUI[6].fillAmount;
+            yield return new WaitForSeconds(waitTime / 2);
+        }
+    }
+
+    private IEnumerator loop9()
+    {
+        yield return new WaitForSeconds(0.06f);
+        while (true)
+        {
+            displayComboUI[8].fillAmount = displayComboUI[7].fillAmount;
+            yield return new WaitForSeconds(waitTime / 2);
+        }
+    }
+
+    private IEnumerator loop10()
+    {
+        yield return new WaitForSeconds(0.05f);
+        while (true)
+        {
+            displayComboUI[9].fillAmount = displayComboUI[8].fillAmount;
+            yield return new WaitForSeconds(waitTime / 2);
+        }
+    }
+
+    private IEnumerator loop11()
+    {
+        yield return new WaitForSeconds(0.04f);
+        while (true)
+        {
+            displayComboUI[10].fillAmount = displayComboUI[9].fillAmount;
+            yield return new WaitForSeconds(waitTime / 2);
+        }
+    }
+
+    private IEnumerator loop12()
+    {
+        yield return new WaitForSeconds(0.03f);
+        while (true)
+        {
+            displayComboUI[11].fillAmount = displayComboUI[10].fillAmount;
+            yield return new WaitForSeconds(waitTime / 2);
+        }
+    }
+
+    private IEnumerator loop13()
+    {
+        yield return new WaitForSeconds(0.02f);
+        while (true)
+        {
+            displayComboUI[12].fillAmount = displayComboUI[11].fillAmount;
+            yield return new WaitForSeconds(waitTime / 2);
+        }
+    }
+
+    private IEnumerator loop14()
+    {
+        yield return new WaitForSeconds(0.01f);
+        while (true)
+        {
+            displayComboUI[13].fillAmount = displayComboUI[12].fillAmount;
+            yield return new WaitForSeconds(waitTime / 2);
+        }
+    }
+
+    private IEnumerator loop15()
+    {
+        //yield return new WaitForSeconds(0.01f);
+        while (true)
+        {
+            displayComboUI[10].fillAmount = displayComboUI[9].fillAmount;
+            yield return new WaitForSeconds(waitTime / 2);
+        }
+    }
+
+    private IEnumerator loop20()
+    {
+        yield return new WaitForSeconds(waitTime / 2);
+        while (true)
+        {
             for (int i = 1; i < 6; i++)
             {
                 displayComboUI[i * 2].fillAmount = displayComboUI[i * 2 - 1].fillAmount;
             }
-            yield return new WaitForSeconds(waitTime);
+            yield return new WaitForSeconds(waitTime / 2);
 
             for (int i = 1; i < 6; i++)
             {
                 displayComboUI[i * 2 - 1].fillAmount = displayComboUI[i * 2 - 2].fillAmount;
             }
-            yield return new WaitForSeconds(waitTime);
+            yield return new WaitForSeconds(waitTime / 2);
         }
     }
 
